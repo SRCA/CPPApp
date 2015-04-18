89e1ec7c0719e03803cd3c3f2dd94f78a1b6c7af
reevesand <reevesand@gmail.com>
2015-04-18 12:25:00 -0500

    Initial commit

---
+++ CCPApp/CCPApp/Utilities/LayoutHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CCPApp.Utilities
+{
+	public class LayoutHelper
+	{
+		public static BoxView GetHorizontalLine()
+		{
+			return new BoxView
+			{
+				HeightRequest = 1,
+				Color = Color.Black
+			};
+		}
+
+		public static BoxView GetVerticalSpacing(double heightRequest)
+		{
+			return new BoxView
+			{
+				HeightRequest = heightRequest
+			};
+		}
+
+		public static BoxView GetHorizontalSpacing(double widthRequest)
+		{
+			return new BoxView
+			{
+				WidthRequest = widthRequest
+			};
+		}
+
+	}
+}
